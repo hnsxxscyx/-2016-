6013229fe139a51769c66e610a738523fe9b1d26
不得不错 <hnsxxscyx@gmail.com>
2017-06-08 11:16:09 +0800

    complete

---
+++ JavaScript/动态数据绑定（一）.js
@@ -0,0 +1,18 @@
+function Observer(obj) {
+    let proxy = {}
+    proxy.data = new Proxy(obj, {
+        get: function(target, property) {
+            if (property in target) {
+                console.log('你访问了' + property)
+                return target[property]
+            } else {
+                console.log('没有' + property)
+            }
+        },
+        set: function(target, key, value, receiver) {
+            console.log('你设置了' + key)
+            target[key] = value
+        }
+    })
+    return proxy
+}
